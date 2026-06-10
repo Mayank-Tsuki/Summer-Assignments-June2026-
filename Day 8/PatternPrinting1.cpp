bfe8bd8191dc110df05bda94351581ec77ac83fe
Mayank-god <mrawat24680@gmail.com>
2026-06-10 12:25:22 +0530

    Create PatternPrinting1.cpp

---
+++ Day 8/PatternPrinting1.cpp
@@ -0,0 +1,22 @@
+//Write a program to Print half pyramid pattern. 
+//  * 
+//  * *  
+//  * * *  
+//  * * * *  
+//  * * * * * 
+
+#include<bits//stdc++.h>
+using namespace std;
+int main(){
+    int n ;
+    cout<<"Enter the value of n (height of pyramid) : ";cin>>n;
+
+    for ( int i = 0 ; i < n ; i++){
+        for ( int j = 0 ; j <= i ; j++){
+            cout<<" * ";
+        }    
+        cout<<endl;
+    }
+    
+    return 0;
+}
