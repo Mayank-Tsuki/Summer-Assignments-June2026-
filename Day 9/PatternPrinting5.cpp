a54363448f08ba0110a54a42de1d80adb20a9ad8
Mayank-god <mrawat24680@gmail.com>
2026-06-10 12:25:36 +0530

    Create PatternPrinting5.cpp

---
+++ Day 9/PatternPrinting5.cpp
@@ -0,0 +1,19 @@
+//Write a program to Print reverse star pattern.
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+#include<bits//stdc++.h>
+using namespace std;
+int main(){
+    int n ;
+    cout<<"Enter the value of n (height of pattern) : ";cin>>n;
+
+    for(int i = n ; i >=1 ; i--){
+        for(int j = 1 ; j <= i ; j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+}
